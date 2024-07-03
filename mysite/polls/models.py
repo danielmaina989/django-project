e4b909c205de14f7b458579cd7ada7a9988c5618
Daniel Maina <danielmaina989@gmail.com>
2024-07-03 15:45:46 +0300

    mysite

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from django.contrib import admin
 import datetime
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 
 # Create your models here.
 class Question(models.Model):
@@ -24,6 +26,10 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+# class Voter(models.Model):
+#     user = models.ForeignKey(User)
+#     poll = models.ForeignKey(Question)
 
 def was_published_recently(self):
     now = timezone.now()
