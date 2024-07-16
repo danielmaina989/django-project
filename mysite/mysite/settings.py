c4b4729783e4a1ab13c585a3967dfb4f38d7d75f
Daniel Maina <danielmaina989@gmail.com>
2024-07-16 14:06:41 +0300

    mysite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -128,8 +128,8 @@ LOGIN_URL="login"
 
 # email configs
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'mail.electricflirt.com'
+EMAIL_USE_TLS = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
+EMAIL_HOST_USER = 'lilliana@electricflirt.com'
+EMAIL_HOST_PASSWORD = 'iJIzolJLhTXxdg=='
